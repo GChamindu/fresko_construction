35b819369f13e6d88943ecaa23653436754dff17
Gavishka <gavishka.rightmo@gmail.com>
2025-05-14 23:33:35 +0530

    refactor(page): update metadata title and description for Home 1

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Home1 from "./(homes)/home-1/page";
 export const metadata = {
-  title: "Home 1 || Xbuild - Constriction nextjs Template",
-  description: "Xbuild - Constriction nextjs Template",
+  title: "Fresko Engineering",
+  description: "Fresko - Constriction",
 };
 export default function Home() {
   return (
